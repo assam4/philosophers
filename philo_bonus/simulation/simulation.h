94b1226c4bbba93a4964510ffd17682a248fed15
Samvel <115891697+assam4@users.noreply.github.com>
2025-04-21 21:42:45 +0400

    Update simulation.h

--- philo_bonus/simulation/simulation.h
+++ philo_bonus/simulation/simulation.h
@@ -21,7 +21,7 @@
 # define THREAD_ERR "\033[31mError creating thread\n\033[0m"
 # define FORK_ERR "\033[31mError creating process\n\033[0m"
 # define GET_FORK "\033[0m[%llu] %i has taken a fork 🍴\n"
-# define EATING "\033[0;32m[%llu] %i is eating 😋\n \033[0m"
+# define EATING "\033[0;32m[%llu] %i is eating 😋\n\033[0m"
 # define SLEEPING "\033[1;33m[%llu] %i is sleeping 😴\n\033[0m"
 # define THINKING "\033[0;33m[%llu] %i is thinking 🤔\n\033[0m"
 # define PHILO_DEAD "\033[38;5;88m[%llu] %i is dead 💀\033[0m\n"
